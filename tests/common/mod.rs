87c056a05ead268e8302906249eab924739acd8b
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-04 07:20:39 +0900

    Add log directory test helper

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,20 @@
 use std::fs;
 use std::ops::Deref;
+use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
 use std::time::{SystemTime, UNIX_EPOCH};
+
+pub(crate) const LOG_DIRECTORY_MARKER: &str = ".logcut-directory";
+pub(crate) const LOG_DIRECTORY_MARKER_CONTENT: &str = "logcut log directory\n";
+
+pub(crate) fn prepare_log_directory(path: &Path) {
+    fs::create_dir_all(path).unwrap();
+    fs::set_permissions(path, fs::Permissions::from_mode(0o700)).unwrap();
+
+    let marker = path.join(LOG_DIRECTORY_MARKER);
+    fs::write(&marker, LOG_DIRECTORY_MARKER_CONTENT).unwrap();
+    fs::set_permissions(marker, fs::Permissions::from_mode(0o600)).unwrap();
+}
 
 pub(crate) struct TestDir {
     path: PathBuf,
